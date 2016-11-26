783c73b5009bd25f419c8fcdb9081c3b415b19f3
José Olórtegui <jolortegui98@gmail.com>
2016-11-26 20:07:00 -0300

    Ajustes en index y filtros

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,24 @@
+// WOW
+new WOW().init();
+
 $(document).ready(function(){
+	// HAMBURGUESA
 	$('.hamburger').click(function(){
 		$(this).toggleClass('is-active');
+		$('.header__menu').slideDown(600);
+
+		if (this.className === "hamburger hamburger--collapse") {
+			$('.header__menu').slideUp(600);
+		}
+	});
+
+	// SLIDER
+	$('.slider').unslider({
+		autoplay: 	true,
+		keys: 		false,
+		arrows: 	false,
+		nav:		false,
+		delay:		5000,	// default --> 3000
+		infinite:	true
 	});
 });
