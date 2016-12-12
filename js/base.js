be330999417b8c26e04f6278f7a4efe05cddec7c
Alcides Benitez <alcides.abel.benitez@gmail.com>
2016-12-12 03:59:33 -0300

    Agregado separador de miles en la pestaña de funcionarios y alineado sus columnas numericas a la derecha y las demas centradas si lo requieren. También se alinearon las columnas del datatable de la pestaña municipios.

--- js/base.js
+++ js/base.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 	
+	function numeroEntero(numero){
+		return numero.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+	}
+	
 	var funcionarios = $.ajax({
 		  url: '/hackathon2016/php/funcionarios.php',
 	      type:'get',
@@ -10,18 +14,18 @@ $(document).ready(function(){
 
     var cuerpo = '';
     for(var m = 0; m < funcionarios.length; m++){
-        cuerpo += "<tr><td>"+funcionarios[m].anho+"</td>"+
-        "<td>"+funcionarios[m].mes+"</td>"+
+        cuerpo += "<tr><td class='dt-body-center'>"+funcionarios[m].anho+"</td>"+
+        "<td class='dt-body-center'>"+funcionarios[m].mes+"</td>"+
         "<td>"+funcionarios[m].descripcionOee+"</td>"+
-        "<td>"+funcionarios[m].documento+"</td>"+
+        "<td class='dt-body-right'>"+numeroEntero(funcionarios[m].documento)+"</td>"+
         "<td>"+funcionarios[m].nombres+"</td>"+
         "<td>"+funcionarios[m].apellidos+"</td>"+
-        "<td>"+funcionarios[m].sexo+"</td>"+
-        "<td>"+funcionarios[m].presupuestado+"</td>"+
-        "<td>"+funcionarios[m].devengado+"</td>"+        
-        "<td>"+funcionarios[m].estado+"</td>"+
-        "<td>"+funcionarios[m].anhoIngreso+"</td>"+
-        "<td>"+funcionarios[m].discapacidad+"</td>";
+        "<td class='dt-body-center'>"+funcionarios[m].sexo+"</td>"+
+        "<td class='dt-body-right'>"+numeroEntero(funcionarios[m].presupuestado)+"</td>"+        
+        "<td class='dt-body-right'>"+numeroEntero(funcionarios[m].devengado)+"</td>"+        
+        "<td class='dt-body-center'>"+funcionarios[m].estado+"</td>"+
+        "<td class='dt-body-right'>"+funcionarios[m].anhoIngreso+"</td>"+
+        "<td class='dt-body-center'>"+funcionarios[m].discapacidad+"</td>";
         if(funcionarios[m].cargaHoraria == null){
         	 cuerpo += "<td></td>";
         }else{
@@ -35,7 +39,13 @@ $(document).ready(function(){
        }       
     }
     $('#cuerpoFuncionarios').append(cuerpo);
-    $('#datatableFuncionarios').dataTable();
+    $('#datatableFuncionarios').dataTable(
+//    {
+//		"formatNumber": function ( toFormat ) {
+//		    return toFormat.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "'");
+//		};
+//    }
+    );
     
     var cuerpoFuncionariosDobleRemunerados = '';
     for(var k = 100; k <= 500; k=k+100){
